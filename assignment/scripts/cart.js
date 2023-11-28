9ccbf4a238a02904b115772b7825e2dc083bc803
Riley DuBord <thelifeofdu@gmail.com>
2023-11-28 11:54:40 -0600

    first function complete, needs a check

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,9 +1,13 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
 
-
-
+function addItem(item){
+    basket.push(item);
+    return true;
+}
+console.log(addItem("apples"))
 
 
 
