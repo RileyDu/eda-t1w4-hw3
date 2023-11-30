7ae5859fd72da7681f2ca37d6cce6d36837d1e3e
Riley DuBord <thelifeofdu@gmail.com>
2023-11-30 10:25:11 -0600

    added items and logs

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,38 +1,38 @@
-console.log('***** Cart Functions *****');
+console.log("***** Cart Functions *****");
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 let basket = [];
 
-function addItem(item){
-    basket.push(item);
-    return true;
+function addItem(item) {
+  basket.push(item);
+  return true;
 }
-console.log('Was the item added?', addItem("apples"))
-console.log('What is in the basket now?', basket);
+console.log("Was the item added?", addItem("apples"));
+console.log("What is in the basket now?", basket);
+console.log("Was the item added?", addItem("pears"));
+addItem('watermelon');
+addItem('limes')
 
-function listItems(basket){
-    for (item of basket){
-        console.log(item);
-    }
+function listItems(basket) {
+  for (item of basket) {
+    console.log(item);
+  }
 }
-console.log(listItems(basket))
-
-
-
-
+listItems(basket);
+console.log(basket);
 
 // DO NOT MODIFY
 // Used for automated testing
 try {
-    module.exports = {
-        basket: typeof basket !== 'undefined' ? basket : undefined,
-        addItem: typeof addItem !== 'undefined' ? addItem : undefined,
-        listItems: typeof listItems !== 'undefined' ? listItems : undefined,
-        maxItems: typeof maxItems !== 'undefined' ? maxItems : undefined,
-        empty: typeof empty !== 'undefined' ? empty : undefined,
-        isFull: typeof isFull !== 'undefined' ? isFull : undefined,
-        removeItem: typeof removeItem !== 'undefined' ? removeItem : undefined,
-    };
-} catch(e) {
-    // Do nothing
+  module.exports = {
+    basket: typeof basket !== "undefined" ? basket : undefined,
+    addItem: typeof addItem !== "undefined" ? addItem : undefined,
+    listItems: typeof listItems !== "undefined" ? listItems : undefined,
+    maxItems: typeof maxItems !== "undefined" ? maxItems : undefined,
+    empty: typeof empty !== "undefined" ? empty : undefined,
+    isFull: typeof isFull !== "undefined" ? isFull : undefined,
+    removeItem: typeof removeItem !== "undefined" ? removeItem : undefined,
+  };
+} catch (e) {
+  // Do nothing
 }
